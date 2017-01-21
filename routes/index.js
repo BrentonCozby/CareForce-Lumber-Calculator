2aef741293d7e552be91d3d8c96553a459b98648
Brenton Cozby <brenton.cozby@gmail.com>
2017-01-21 17:24:17 -0500

    fix route path in index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/projects/CareForce-Lumber-Calculator', function(req, res, next) {
     res.render('index', {
         title: 'Care Force Lumber Calculator',
         description: 'Optimizes lumber orders and provides cut lists for various City Year Care Force schematics.'
