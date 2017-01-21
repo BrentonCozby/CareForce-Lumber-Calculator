aee11998443b284621ae7708ab57237c44c970b8
Brenton Cozby <brenton.cozby@gmail.com>
2017-01-21 17:03:49 -0500

    serve from server.js

--- server.js
+++ server.js
@@ -51,4 +51,8 @@ app.use(function(err, req, res) {
     res.render('error');
 });
 
-module.exports = app;
+app.listen(3001, () => {
+    console.log(`Listening on port 3001...`);
+});
+
+// module.exports = app;
