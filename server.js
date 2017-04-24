f7e1d602d951c67cfc8bfd48121eabc761388fb7
Brenton Cozby <brenton.cozby@gmail.com>
2017-04-24 10:47:53 -0400

    shake the tree

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const express = require('express');
-// const h5bp = require('h5bp');
 const path = require('path');
 const favicon = require('serve-favicon');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 
 // Define Routes
 const index = require('./routes/index');
 
-// Create express app with html5 boilerplate
 const app = express();
-// app.use(h5bp({
-//     root: path.join(__dirname, 'public'),
-//     processor: 'commonjs'
-// }));
+const PORT = process.env.PORT || 3000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,11 +16,6 @@ app.set('view engine', 'pug');
 // Middleware
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Use routes
@@ -51,4 +39,6 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-module.exports = app;
+app.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}...`);
+})
