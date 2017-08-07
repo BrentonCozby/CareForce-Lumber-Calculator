44789c7c52875a90a99dadbb6727b8246cce8f2e
Brenton Cozby <brenton.cozby@gmail.com>
2017-08-06 20:27:06 -0400

    update root path

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-const ROOT_PATH = (process.env.npm_lifecycle_event === 'prod')
+const ROOT_PATH = (process.env.npm_lifecycle_event === 'build-js-min')
     ? '/CareForce-Lumber-Calculator/'
     : '/'
+
+console.log('ROOT_PATH', ROOT_PATH);
 
 exports.ROOT_PATH = ROOT_PATH
 exports.SITE_TITLE = 'Care Force Lumber Calculator'
